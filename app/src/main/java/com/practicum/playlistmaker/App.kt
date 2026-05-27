5f9153cf531e3238dbe164ef86198c8bd6f039fc
Timon <Timon994@yandex.ru>
2026-05-28 00:13:21 +0300

    [17] Koin

--- app/src/main/java/com/practicum/playlistmaker/App.kt
+++ app/src/main/java/com/practicum/playlistmaker/App.kt
@@ -1,15 +1,28 @@
 package com.practicum.playlistmaker
 
 import android.app.Application
-import com.practicum.playlistmaker.creator.Creator
+import com.practicum.playlistmaker.di.appModule
+import com.practicum.playlistmaker.di.playerModule
+import com.practicum.playlistmaker.di.viewModelModule
+import com.practicum.playlistmaker.settings.domain.SettingsInteractor
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import org.koin.java.KoinJavaComponent.get
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        Creator.initialize(this)
+        startKoin {
+            androidContext(this@App)
+            modules(
+                appModule,
+                playerModule,
+                viewModelModule
+            )
+        }
 
-        val settingsInteractor = Creator.provideSettingsInteractor()
+        val settingsInteractor: SettingsInteractor = get(SettingsInteractor::class.java)
         settingsInteractor.applyCurrentTheme()
     }
 }
--- app/src/main/java/com/practicum/playlistmaker/player/data/PlayerRepositoryImpl.kt
+++ app/src/main/java/com/practicum/playlistmaker/player/data/PlayerRepositoryImpl.kt
@@ -4,10 +4,16 @@ import android.media.MediaPlayer
 import com.practicum.playlistmaker.player.domain.PlayerRepository
 
 class PlayerRepositoryImpl(
-    private val mediaPlayer: MediaPlayer
+    private var mediaPlayer: MediaPlayer
 ) : PlayerRepository {
 
+    private var isReleased = false
+
     override fun prepare(url: String, onPrepared: () -> Unit) {
+        if (isReleased) {
+            mediaPlayer = MediaPlayer()
+            isReleased = false
+        }
         mediaPlayer.apply {
             setDataSource(url)
             prepareAsync()
@@ -15,12 +21,36 @@ class PlayerRepositoryImpl(
         }
     }
 
-    override fun start() = mediaPlayer.start()
-    override fun pause() = mediaPlayer.pause()
-    override fun release() = mediaPlayer.release()
-    override fun getCurrentPosition(): Int = mediaPlayer.currentPosition
-    override fun isPlaying(): Boolean = mediaPlayer.isPlaying
+    override fun start() {
+        if (!isReleased) {
+            mediaPlayer.start()
+        }
+    }
+
+    override fun pause() {
+        if (!isReleased) {
+            mediaPlayer.pause()
+        }
+    }
+
+    override fun release() {
+        if (!isReleased) {
+            mediaPlayer.release()
+            isReleased = true
+        }
+    }
+
+    override fun getCurrentPosition(): Int {
+        return if (!isReleased) mediaPlayer.currentPosition else 0
+    }
+
+    override fun isPlaying(): Boolean {
+        return if (!isReleased) mediaPlayer.isPlaying else false
+    }
+
     override fun setOnCompletionListener(listener: () -> Unit) {
-        mediaPlayer.setOnCompletionListener { listener() }
+        if (!isReleased) {
+            mediaPlayer.setOnCompletionListener { listener() }
+        }
     }
 }
